02f792e331c4ee464ce38a5a53726aa29bba0154
Zeppo95 <martial.anselle@laposte.net>
2022-04-06 17:07:51 +0200

    Update

---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use App\Entity\Produit;
+use App\Form\ProduitType;
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+/**
+ * @Route("/admin")
+ */
+class ProduitController extends AbstractController
+{
+
+    /**
+     *@Route("/voir-les-produits", name="show_produit", methods={"GET"})
+     */
+    public function showProduit(ProduitRepository $produitRepository): Response
+    {
+        return $this->render("admin/show_produit.html.twig", [
+            'produits' =>$produitRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/creer-un-produit", name="create_produit", methods={"GET|POST"}) 
+     */
+    public function createProduit(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+        $produit = new Produit();
+
+        $form = $this->createForm(ProduitType::class, $produit)->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $produit->setCreatedAt(new DateTime());
+            $produit->setUpdatedAt(new DateTime());
+
+            $photo = $form->get('photo')->getData();
+
+            if($photo) {
+
+                $extension = '.' . $photo->guessExtension();
+                $safeFilename = $slugger->slug($produit->getTitle());
+
+                $newFilename = $safeFilename . '_' . uniqid() . $extension;
+
+                try{
+                    $photo->move($this->getParameter('uploads_dir'), $newFilename);
+                    $produit->setPhoto($newFilename);
+                } catch (FileException $exception) {
+                    $this->addFlash('warning', 'La photo du produit n\'a pas pu être importée, veuillez réessayer en modifiant le produit.');
+                    //return $this->redirectToRoute('show_produit');
+                } // end catch()
+
+            } // end if($photo)
+
+            $entityManager->persist($produit);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le nouveau produit est en ligne !');
+            return $this->redirectToRoute('show_produit');
+            
+        } // end if($form)
+
+        return $this->render('admin/form/form_produit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    
+    /**
+     * @Route("/modifier-un-produit_{id}", name="update_produit", methods={"GET|POST"})
+     */
+    public function updateProduit(Produit $produit, EntityManagerInterface $entityManager, Request $request, SluggerInterface $slugger): Response
+    {
+        $originalPhoto = $produit->getPhoto();
+
+        $form = $this->createForm(ProduitType::class, $produit, [
+            'photo' => $originalPhoto
+        ])->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $produit->setUpdatedAt(new DateTime());
+
+            $photo = $form->get('photo')->getData();
+
+            if($photo) {
+                $this->handleFile($produit, $photo, $slugger);
+            } else {
+                $produit->setPhoto($originalPhoto);
+            }
+
+            $entityManager->persist($produit);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le produit a bien été modifié !');
+            return $this->redirectToRoute('show_produit');
+        }
+
+        return $this->render('admin/form/form_produit.html.twig', [
+            'form' => $form->createView(),
+            'produit' => $produit,
+        ]);
+    }
+
+    private function handleFile(Produit $produit, UploadedFile $photo, SluggerInterface $slugger)
+    {
+        $extension = '.' . $photo->guessExtension();
+        $safeFilename = $slugger->slug($produit->getTitle());
+
+        $newFilename = $safeFilename . '_' . uniqid() . $extension;
+
+        try{
+                    $photo->move($this->getParameter('uploads_dir'), $newFilename);
+                    $produit->setPhoto($newFilename);
+                } catch (FileException $exception) {
+                    $this->addFlash('warning', 'La photo du produit n\'a pas pu être importée, veuillez réessayer en modifiant le produit.');
+                    //return $this->redirectToRoute('show_produit');
+                } // end catch()
+    }
+
+}
